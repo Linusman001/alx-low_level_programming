af075d3052db7b588221a63b89ef4ed3f254c55c
Linus Chidera <Linuschidera17@gmail.com>
2022-06-03 14:55:33 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print letters in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{ char alphabet;
+
+	while (alphabet = 'a', alphabet <= 'z', alphabet++)
+	{
+		putchar(alphabets);
+
+		putchar("\n");
+	}
+
+	return (0);
+}
