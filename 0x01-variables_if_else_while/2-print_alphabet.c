8750ac3a8dc3b7d3b073014c7a94483661b9adcc
Linus Chidera <Linuschidera17@gmail.com>
2022-06-03 15:40:31 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,9 @@ int main(void)
 {
 	char alphabet;
 
-	alphabet = 'a';
-		while (alphabet <= 'z')
-			putchar(alphabet);
-			alphabet++;
-			putchar('\n');
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
 
-		return (0);
+	return (0);
 }
