0926b9626b23261c57c80e2e3ac0214ab1af9f77
Linus Chidera <Linuschidera17@gmail.com>
2022-06-03 16:47:13 -0700

    5 5 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit number fromm zero
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++
+	}
+
+	return (0);
+}
