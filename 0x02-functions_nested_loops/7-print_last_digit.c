386dff96456d4b2793443592f373b0aae856bc7a
Linus Chidera <Linuschidera17@gmail.com>
2022-06-07 18:52:50 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,10 @@
  */
 int print_last_digit(int a)
 {
-	int last_digit = n % 10;
+	int last_digit = a % 10;
 
-	if ((a % 10) < 0)
-		a % 10 *= -1;
+	if (last_digit < 0)
+		last_digit *= -1;
 	_putchar(last_digit + '0');
 
 	return (last_digit);
