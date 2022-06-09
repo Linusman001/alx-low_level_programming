32f0c0dd77c682ad69c61bec61872f988e0f0797
Linus Chidera <Linuschidera17@gmail.com>
2022-06-09 16:43:00 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 - 9
+ * followed by a new line
+ * Return: 0
+ */
+void print_most-numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '0'; a++)
+	{
+		if (a != '2'&& a != '4')
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
