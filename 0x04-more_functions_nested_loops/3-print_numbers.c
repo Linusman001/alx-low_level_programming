991d742471e7e1b9fcde864958664bf877830171
Linus Chidera <Linuschidera17@gmail.com>
2022-06-09 16:08:40 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - a function that prints a number from 0 to 9
  * followed by a new line
- * Return: 0
+ * Return: Nothing
  */
 void print_numbers(void)
 {
@@ -11,7 +11,7 @@ void print_numbers(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar(a)
+		_putchar(a);
 	}
 	_putchar('\n');
 }
