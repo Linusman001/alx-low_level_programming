7a1b5fe3c740cf27d0378422e7c1db549751bd10
Linus Chidera <Linuschidera17@gmail.com>
2022-06-13 14:42:52 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: variable to be checked
+ * Return: 0
+ */
+int _strlen(char *s);
+{
+	strlen(*s);
+	return (0);
+}
