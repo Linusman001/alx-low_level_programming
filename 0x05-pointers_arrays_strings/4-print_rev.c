8d57d2f96816b03aa76323467f044242c5b51f03
Linus Chidera <Linuschidera17@gmail.com>
2022-06-13 17:23:14 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,8 +8,7 @@
  */
 void print_rev(char *s)
 {
-	int count;
-	count = strlen(s);
+	int count = 0;
 
 	while (count >= 0)
 	{
@@ -17,7 +16,6 @@ void print_rev(char *s)
 			break;
 		count++;
 	}
-
 	for (count--; count >= 0; count--)
 		_putchar(s[count]);
 	_putchar('\n');
