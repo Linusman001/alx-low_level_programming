17faa7d501bd7a9f912ad32ce65bb65c7970eade
Linus Chidera <Linuschidera17@gmail.com>
2022-06-24 15:44:42 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints its name
+ * followed by a new line
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
